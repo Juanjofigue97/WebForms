8603114249ac5aabc3fb0f0588f825e207a96c84
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2022-09-13 15:53:59 -0500

    Update my.js

--- my.js
+++ my.js
@@ -1,4 +1,5 @@
 
+
 function testGS(){
     
     const url = "https://script.google.com/macros/s/AKfycbyP3Xu4-PeAAZebALYT9Do3V4nFC8chxzcw3yBOm6cx9Kw5k_A/exec";
@@ -11,4 +12,29 @@ function testGS(){
 
 }
 
+function adddGS(){
+    
+    const url = "https://script.google.com/macros/s/AKfycbyP3Xu4-PeAAZebALYT9Do3V4nFC8chxzcw3yBOm6cx9Kw5k_A/exec";
+    
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'omit', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name: "Olivia"}) // body data type must match "Content-Type" header
+    });
+}
+
+
+    
+document.getElementById("btn2").addEventListener("click",adddGS)
+
 document.getElementById("btn").addEventListener("click",testGS)
+
+// https://script.google.com/macros/s/AKfycbyP3Xu4-PeAAZebALYT9Do3V4nFC8chxzcw3yBOm6cx9Kw5k_A/exec
